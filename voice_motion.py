833fd0680700074aa54104fbbc1ec7905e9ea132
Uzzy101 <114440609+Uzzy101@users.noreply.github.com>
2023-08-29 09:04:46 +0100

    Add files via upload

--- voice_motion.py
+++ voice_motion.py
@@ -11,14 +11,32 @@ motor1B = 18
 motor2A = 22
 motor2B = 23
 
+# arm commands
+pick_motor = 25
+drop_motor = 5
+
 # Set motor pins as output
 GPIO.setup(motor1A, GPIO.OUT)
 GPIO.setup(motor1B, GPIO.OUT)
 GPIO.setup(motor2A, GPIO.OUT)
 GPIO.setup(motor2B, GPIO.OUT)
+GPIO.setup(pick_motor, GPIO.OUT)
+GPIO.setup(drop_motor, GPIO.OUT)
 
 # Initialize speech recognition
 recognizer = sr.Recognizer()
+
+
+# Function to pick an object
+def pick_object():
+    # print('Picking object')
+    GPIO.setup(pick_motor, GPIO.LOW)
+
+
+def drop_object():
+    # print('Dropping object')
+    GPIO.setup(drop_motor, GPIO.LOW)
+
 
 # Function to move the robot forward
 def move_forward():
@@ -27,12 +45,14 @@ def move_forward():
     GPIO.output(motor2A, GPIO.HIGH)
     GPIO.output(motor2B, GPIO.LOW)
 
+
 # Function to move the robot backward
 def move_backward():
     GPIO.output(motor1A, GPIO.LOW)
     GPIO.output(motor1B, GPIO.HIGH)
     GPIO.output(motor2A, GPIO.LOW)
     GPIO.output(motor2B, GPIO.HIGH)
+
 
 # Function to turn the robot left
 def turn_left():
@@ -41,6 +61,7 @@ def turn_left():
     GPIO.output(motor2A, GPIO.HIGH)
     GPIO.output(motor2B, GPIO.LOW)
 
+
 # Function to turn the robot right
 def turn_right():
     GPIO.output(motor1A, GPIO.HIGH)
@@ -48,12 +69,14 @@ def turn_right():
     GPIO.output(motor2A, GPIO.LOW)
     GPIO.output(motor2B, GPIO.HIGH)
 
+
 # Function to stop the robot
 def stop_robot():
     GPIO.output(motor1A, GPIO.LOW)
     GPIO.output(motor1B, GPIO.LOW)
     GPIO.output(motor2A, GPIO.LOW)
     GPIO.output(motor2B, GPIO.LOW)
+
 
 try:
     while True:
@@ -65,8 +88,12 @@ try:
         try:
             command = recognizer.recognize_google(audio).lower()
             print("Command:", command)
-            
-            if "move forward" in command:
+
+            if 'pick object' in command:
+                pick_object()
+            elif 'drop object' in command:
+                drop_object()
+            elif "move forward" in command:
                 move_forward()
             elif "move backward" in command:
                 move_backward()
